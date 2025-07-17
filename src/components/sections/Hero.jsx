d3364839f430e4089d615c671e31ba64ebe649ad
Prathikson <prathik@vincellstudios.com>
2025-07-16 19:18:20 -0600

    scroll fix3

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -71,12 +71,13 @@ const Hero = () => {
       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       borderRadius: "0% 0% 0% 0%",
       ease: "power1.inOut",
-      scrollTrigger: {
-        trigger: "#video-frame",
-        start: "center center",
-        end: "bottom center",
-        scrub: true,
-      },
+scrollTrigger: {
+  trigger: "#video-frame",
+  start: "center center",
+  end: "bottom center",
+  scrub: true,
+  scroller: document.body,
+},
     });
   });
 
