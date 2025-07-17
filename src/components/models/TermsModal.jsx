b178bfc80bcb41cb8f181585c791eccf79d85897
Prathikson <prathik@vincellstudios.com>
2025-07-16 20:17:32 -0600

    fixed index.css to fix the terms and cookie issue

--- src/components/models/TermsModal.jsx
+++ src/components/models/TermsModal.jsx
@@ -15,6 +15,7 @@ const TermsModal = ({ onClose }) => {
 
   return (
     <div
+    
       className="fixed inset-0 z-50 bg-black bg-opacity-60 flex items-center justify-center transition-opacity duration-300"
       onClick={onClose}
     >
