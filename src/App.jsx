88a786223a18948033f3751bfdc3c8974f3fad7a
Prathikson <prathik@vincellstudios.com>
2025-07-16 19:41:50 -0600

    webgl Logo Model Optimized

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Pricing from "./components/sections/Pricing";
 import Contact from "./components/sections/Contact";
 // import Footer from "./components/layout/Footer";
 import Testimonials from "./components/sections/Testimonials";
-// import Test from "./components/sections/Test";
+import Test from "./components/sections/Test";
 import Project from "./components/sections/Project";
 import About from "./components/sections/About";
 import ComparisonSection from "./components/sections/ComparisonSection";
@@ -30,7 +30,7 @@ function App() {
         <Hero />
         <About />
         <Featutres />
-        {/* <Test /> */}
+        <Test />
         <Project />
         <Story />
         <ComparisonSection />
